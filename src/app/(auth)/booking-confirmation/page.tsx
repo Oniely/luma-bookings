e455f229be4de055bef79db0f7145d3f4fbaf5c4
Oniely <jabagatniel09@gmail.com>
2025-03-24 23:17:33 +0800

    sff

--- src/app/(auth)/booking-confirmation/page.tsx
+++ src/app/(auth)/booking-confirmation/page.tsx
@@ -3,7 +3,7 @@ import BookingConfirmation from "@/components/Forms/BookingConfirmation";
 const BookingConfirmationPage = () => {
 	return (
 		<div className="padding-container max-container">
-			<BookingConfirmatio n />
+			<BookingConfirmation />
 		</div>
 	);
 };
