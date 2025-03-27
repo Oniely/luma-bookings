07f913b6fbb863cbdecd85f5be5215e783a37794
tomasu-takahashi <takahashitomasu@gmail.com>
2025-03-27 16:01:54 +0800

    edited navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,11 +8,7 @@ const Navbar = () => {
 	return (
 		<div className="flexBetween w-full h-[5rem] padding-container max-container absolute top-0 z-30 left-1/2 tranform -translate-x-1/2 text-white bg-black/30">
 			<div className="gap-3 flexCenter">
-				<Link href="/rooms">
-					<Button className="font-semibold text-current">
-						Book Now
-					</Button>
-				</Link>
+				<Button className="font-semibold text-current">Book Now</Button>
 			</div>
 			<div className="absolute text-3xl transform -translate-x-1/2 font-canela-light left-1/2 flexCenter">
 				<Link href="/">
@@ -27,7 +23,7 @@ const Navbar = () => {
 				</Link>
 			</div>
 			<div className="gap-3 text-current flexCenter">
-				<Searchbar />
+				
 				<Menu />
 			</div>
 		</div>
